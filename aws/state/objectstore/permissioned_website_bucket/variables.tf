0df1b0655a6d8f2bb6459f345e5cf11f1d5bc984
Raphael Luckom <raphaelluckom@gmail.com>
2021-01-05 15:48:03 -0500

    fix website bucket notif

--- aws/state/objectstore/permissioned_website_bucket/variables.tf
+++ aws/state/objectstore/permissioned_website_bucket/variables.tf
@@ -15,9 +15,11 @@ variable "lambda_notifications" {
   type = list(object({
     lambda_arn = string
     lambda_name = string
+    lambda_role_arn = string
     events = list(string)
     filter_prefix = string
     filter_suffix = string
+    permission_type = string
   }))
   default = []
 }
