6517ff94c32bd32fcdf1c79f393b9c8726a4b303
Raphael Luckom <raphaelluckom@gmail.com>
2021-02-12 14:13:45 -0500

    logger update

--- aws/layers/cognito_utils/layer.tf
+++ aws/layers/cognito_utils/layer.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "layer" {
   layer_name = var.layer_name
   s3_bucket = "rluckom-public-layer-archives"
   s3_key = "cognito_utils"
-  s3_object_version = "eC9ZgrzPsk58tSIgJEhSlPfntNrgsnCY"
+  s3_object_version = "ixzOf5ihvBoFqMILfvWzGDXJdvYZnISx"
   compatible_runtimes = ["nodejs12.x"]
   lifecycle {
     create_before_destroy = true
