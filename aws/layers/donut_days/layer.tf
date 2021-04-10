f52d499c9de1b7244c78a079e7367390557e6804
Raphael Luckom <raphaelluckom@gmail.com>
2021-04-10 19:34:45 -0400

    bump exploranda

--- aws/layers/donut_days/layer.tf
+++ aws/layers/donut_days/layer.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "layer" {
   layer_name = var.layer_name
   s3_bucket = "rluckom-public-layer-archives"
   s3_key = "donut_days"
-  s3_object_version = "O09iyGgsmBxSFR03jY1d6n0E.Ce9rd3X"
+  s3_object_version = "TUAyZ2X.8n4VG9TmC4UYzdczYHRHhWWa"
   compatible_runtimes = ["nodejs12.x"]
   lifecycle {
     create_before_destroy = true
