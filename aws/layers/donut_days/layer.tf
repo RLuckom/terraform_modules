262441259bc0d52458edc51a094bd6189c3c4d62
Raphael Luckom <raphaelluckom@gmail.com>
2021-04-10 12:30:50 -0400

    remove exp peerdeps

--- aws/layers/donut_days/layer.tf
+++ aws/layers/donut_days/layer.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "layer" {
   layer_name = var.layer_name
   s3_bucket = "rluckom-public-layer-archives"
   s3_key = "donut_days"
-  s3_object_version = "oczkrHBbY8cftixMs_itYd6vq7CqM6Yf"
+  s3_object_version = "O09iyGgsmBxSFR03jY1d6n0E.Ce9rd3X"
   compatible_runtimes = ["nodejs12.x"]
   lifecycle {
     create_before_destroy = true
