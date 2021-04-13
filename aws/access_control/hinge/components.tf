7224489d622bb20c49234a8ad6b1f5013128c2af
Raphael Luckom <raphaelluckom@gmail.com>
2021-04-13 16:51:05 -0400

    comment

--- aws/access_control/hinge/components.tf
+++ aws/access_control/hinge/components.tf
@@ -18,6 +18,19 @@ resource "aws_cognito_identity_pool_roles_attachment" "main" {
     ambiguous_role_resolution = "Deny"
     type                      = "Rules"
 
+    // When a getCredentialsForIdentity request is received by aws,
+    // if it HAS a CustomRoleArn argument AND the identity is eligible for the role,
+    // credentials for the role are returned.
+    //
+    // If it DOES NOT have a CustomRoleArn argument, aws returns the FIRST role
+    // for which it is eligible (based on mapping rule order), or else the behavior
+    // specified in ambiguous_role_resolution.
+    //
+    // That means that if we include a role with no permissions as the first rule,
+    // and we make it so that all valid tokens are eligible for that role (by testing
+    // whether the audience matches our client ID, which it will for all valid tokens)
+    // then any getCredentialsForIdentity request that does NOT specify a CustomRoleArn
+    // will be given the empty role rather than an arbitrary one.
     mapping_rule {
       claim      = "aud"
       match_type = "Equals"
