c41fb2d71cc280fbdda532a57a8f316ff53df059
Raphael Luckom <raphaelluckom@gmail.com>
2021-05-05 10:17:52 -0400

    add auth domain to default admin connect srcs

--- aws/state/user_mgmt/stele/outputs.tf
+++ aws/state/user_mgmt/stele/outputs.tf
@@ -13,3 +13,9 @@ output user_pool_client {
 output user_pool_domain {
   value = aws_cognito_user_pool_domain.domain
 }
+
+// safe from circular references,
+// unlike above
+output auth_domain_name {
+  value = local.cognito_domain
+}
