2fc34aac5cbe88c92757fa86133d5d7fa187019e
Raphael Luckom <raphaelluckom@gmail.com>
2021-06-02 10:39:58 -0400

    add breadcrumb

--- themes/admin_site_ui/src/scripts.js
+++ themes/admin_site_ui/src/scripts.js
@@ -13,6 +13,23 @@ const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
+window.addEventListener('load', () => {
+  try {
+    const name = new RegExp(`https://${window.location.host}/plugins/(.*)/(.*).html`).exec(window.location.href)[1]
+    if (name) {
+      const s = document.createElement('span')
+      s.innerText = '::'
+      s.className = "location-link-sep"
+      document.getElementById('location-links').appendChild(s)
+      const a = document.createElement('a')
+      a.href = `/plugins/${name}/index.html`
+      a.className = 'nav-logo'
+      a.innerText = name
+      document.getElementById('location-links').appendChild(a)
+    }
+  } catch(e) {}
+})
+
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
