8b6217e393b02a5283aa7b527ff69fcca2bdcc03
Raphael Luckom <raphaelluckom@gmail.com>
2022-11-10 02:31:02 -0500

    add fn version header

--- snapshots/protocols/boundary_oauth/src/http_headers.js
+++ snapshots/protocols/boundary_oauth/src/http_headers.js
@@ -9,17 +9,22 @@ const getResponseHeaders = require("./shared/shared").getResponseHeaders
 
 let CONFIG
 
+const AWS_LAMBDA_FUNCTION_VERSION = process.env.AWS_LAMBDA_FUNCTION_VERSION
 async function handler (event) {
   if (!CONFIG) {
-    CONFIG = getConfigJson();
-    CONFIG.logger.debug("Configuration loaded:", CONFIG);
+    CONFIG = getConfigJson()
+    CONFIG.logger.debug("Configuration loaded:", CONFIG)
   }
-  CONFIG.logger.debug("Event:", event);
-  const response = event.Records[0].cf.response;
-  Object.assign(response.headers, getResponseHeaders(event, CONFIG));
-  CONFIG.logger.debug("Returning response:\n", response);
-  return response;
-};
+  CONFIG.logger.debug("Event:", event)
+  const response = event.Records[0].cf.response
+  Object.assign(response.headers, getResponseHeaders(event, CONFIG))
+  Object.assign(response.headers, {"x-function-version": [{
+    key: 'x-function-version',
+    value: AWS_LAMBDA_FUNCTION_VERSION}]
+  })
+  CONFIG.logger.debug("Returning response:\n", response)
+  return response
+}
 
 module.exports = {
   handler
