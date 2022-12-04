97fb409aa681da0e7023e44badf5e45ad2db0e75
Raphael Luckom <raphaelluckom@gmail.com>
2022-12-04 02:47:23 -0500

    add axios to activitypub utils layer

--- snapshots/aws/layers/activitypub_utils/layer.tf
+++ snapshots/aws/layers/activitypub_utils/layer.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_layer_version" "layer" {
   layer_name = var.layer_name
   s3_bucket = "rluckom-public-layer-archives"
   s3_key = "activitypub_utils"
-  s3_object_version = "CKi_m00z_IEfJ7bHKwfKC7mq0F8mIezz"
+  s3_object_version = "wvPCLypYr21fqw45vcId1HqTpqwsep6W"
   compatible_runtimes = ["nodejs12.x"]
   lifecycle {
     create_before_destroy = true
